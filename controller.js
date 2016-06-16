f6d5f1cbed66b8340d9c5514e6f74b74a1ab72c8
Cam Beaudoin <Cam Beaudoin>
2016-06-15 23:44:24 -0400

    Added queue and basic functionality

--- controller.js
+++ controller.js
@@ -1,10 +1,31 @@
 "use strict";
 
-var app = angular.module('youtucandj', []);
+var app = angular.module('youtucandj', ['youtube-embed']);
 
 app.controller('ytcdjCtrl', ['$scope', function($scope) {
 
-$scope.videoId = "oyEuk8j8imI"
+$scope.videoId = "xuyCdgpa5sY"
+$scope.theBestVideo = 'oyEuk8j8imI';
 
+$scope.video = {};
+
+$scope.videoQueue = [
+  'Hs4VcI8hfYc',
+  'dQmcaTJ76Mo',
+  'SpZYzjLFkLE'
+];
+
+$scope.$on('youtube.player.ready', function ($event, player) {
+  player.playVideo();
+})
+
+$scope.pushNextVideo = function(video) {
+  $scope.videoQueue.push(video)
+}
 
 }])
+
+
+
+
+// https://github.com/brandly/angular-youtube-embed
