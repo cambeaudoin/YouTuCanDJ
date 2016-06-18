24c29a0890e608eb14955c09ae2b93f4f875a583
Cam Beaudoin <cam@flashstock.com>
2016-06-17 21:17:41 -0400

    got videos to play concurrently

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -2,26 +2,86 @@
 
 app.controller('ytcdjCtrl', ['$scope', function($scope) {
 
-$scope.videoId = "xuyCdgpa5sY"
-$scope.theBestVideo = 'oyEuk8j8imI';
+var videoPlaying = false;
 
-$scope.video = {};
+$scope.video1 = null;
+$scope.video2 = null;
+
+
+$scope.video = {
+  videoId: ''
+};
+
+$scope.playerArray = [];
 
 $scope.videoQueue = [
-  'Hs4VcI8hfYc',
-  'dQmcaTJ76Mo',
-  'SpZYzjLFkLE'
+  // 'Hs4VcI8hfYc',
+  // 'dQmcaTJ76Mo',
+  // 'SpZYzjLFkLE'
 ];
 
+// console.log(youtube.player);
+
+// $scope.$watch('videoQueue', function(newVal, oldVal) {
+//       if ( newVal !== oldVal ) {
+
+//   console.log("ping!");
+//   if ($scope.video1 === null) {
+//     $scope.video1 = $scope.videoQueue[0];
+//     $scope.videoQueue.shift();
+//   } else if ($scope.video1 === null) {
+//     $scope.video2 = $scope.videoQueue[0];
+//     $scope.videoQueue.shift();
+//   }
+//       }
+//   // console.log("in here");
+//   // if ($scope.playerArray[1]) {
+//     // playNextVideo($scope.playerArray[0]);
+//     // console.log("called playnext");
+//     // initialLoad();
+//     // console.log("called initialLoad");
+//   // }
+// })
+
 $scope.$on('youtube.player.ready', function ($event, player) {
-  player.playVideo();
+  $scope.playerArray.push(player);
+  console.log($scope.playerArray);
+
+  if (videoPlaying === false) {
+    player.playVideo();
+    videoPlaying = true;
+  }
 })
 
 $scope.pushNextVideo = function(video) {
-  $scope.videoQueue.push(video)
+  console.log(video);
+    if ($scope.video1 === null) {
+    $scope.video1 = video.videoId;
+    $scope.video.videoId = '';
+    console.log($scope.video1);
+  } else if ($scope.video2 === null) {
+    $scope.video2 = video.videoId;
+    $scope.video.videoId = '';
+  } else {
+    $scope.videoQueue.push(video.videoId)
+  }
 }
 
+
+
+
+
+function playNextVideo(video) {
+  // console.log(video);
+  // video.playVideo();
+}
+
+// playNextVideo(playerArray[0])
+
+
+//end
 }])
+
 
 
 
